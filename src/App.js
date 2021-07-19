2dd183766a6b422b50b1b1501608ec2863edaf32
Jakub Vala <jamm3e3333@gmail.com>
2021-07-19 08:30:04 -0400

    updating state properly

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ const App =() => {
   ])
 
   const addExpenseHandler = (newExpenses) => {
-    updateExpenses(expenses.concat(newExpenses));
+    updateExpenses((prevState) => {
+      return [newExpenses, ...prevState];
+    });
   }
   return (
     <div> 
