0e16ffa2fff8e6e4e4523c0353ccf88c37c0ad01
Jam3e3333 <jamm3e3333@gmail.com>
2021-07-20 18:34:09 -0400

    css modules init

--- styling-react/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-react/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -12,12 +12,14 @@ const FormControl = styled.div`
   font-weight: bold;
   display: block;
   margin-bottom: 0.5rem;
+  color: ${props => props.invalid ? 'red' : 'black'}
   }
 
  & input {
   display: block;
   width: 100%;
-  border: 1px solid #ccc;
+  border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+  background: ${props => props.invalid ? 'salmon' : 'transparent'}
   font: inherit;
   line-height: 1.5rem;
   padding: 0 0.25rem;
@@ -27,15 +29,6 @@ const FormControl = styled.div`
   outline: none;
   background: #fad0ec;
   border-color: #8b005d;
-}
-
-&.invalid input {
-  border-color: red;
-  background: salmon;
-}
-
-&.invalid label {
-  color: red;
 }
 `;
 
@@ -61,7 +54,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
