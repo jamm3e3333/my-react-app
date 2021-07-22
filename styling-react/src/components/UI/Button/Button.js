f51194e0c89cfca52d443c0b4a2438b432c59975
Jam3e3333 <jamm3e3333@gmail.com>
2021-07-21 21:07:08 -0400

    adding css modules everywhere

--- styling-react/src/components/UI/Button/Button.js
+++ styling-react/src/components/UI/Button/Button.js
@@ -1,4 +1,5 @@
-import styles from './Button.css';
+import React from 'react';
+import styles from './Button.module.css';
 
 // import styled from 'styled-components';
 
@@ -33,7 +34,7 @@ import styles from './Button.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
