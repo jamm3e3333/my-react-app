b6a01a04cd35670ad01e370984887a18e142d12b
Jam3e3333 <jamm3e3333@gmail.com>
2021-07-28 17:52:35 -0400

    button bumping

--- ordering-app/src/components/Layout/HeaderCartButton.js
+++ ordering-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,34 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
+
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
         return curNumber + item.amount;
-    }, 0)
+    }, 0);
+
+    const buttonClasses = `${classes.button} ${buttonIsHighlighted ?  classes.bump : ''}`;
+
+    useEffect(() => {
+        if(cartCtx.items.length === 0) {
+            return;
+        }
+        setButtonIsHighlighted(true);
+        const cleanTimer = setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(cleanTimer);
+        }
+    }, [cartCtx.items])
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={buttonClasses} onClick={props.onClick}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Your cart</span>
             <span className={classes.badge}>
