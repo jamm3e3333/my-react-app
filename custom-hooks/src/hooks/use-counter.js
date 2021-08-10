ea7cb5dbc5e7260faefb19d16176c0bdafd27821
Jam3e3333 <jamm3e3333@gmail.com>
2021-08-10 08:53:21 -0400

    created custom hooks

--- custom-hooks/src/hooks/use-counter.js
+++ custom-hooks/src/hooks/use-counter.js
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
       const interval = setInterval(() => {
-        setCounter((prevCounter) => prevCounter + 1);
+        if(forwards) {
+          setCounter(prevState => setCounter(prevState + 1));
+        }
+        else {
+          setCounter(prevState => setCounter(prevState - 1));
+        }
       }, 1000);
   
       return () => clearInterval(interval);
-    }, []);
+    }, [forwards]);
   
     return counter;
 }
