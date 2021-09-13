77a5e6146bfb730029784becefb75665a3f41e4c
Jam3e3333 <jamm3e3333@gmail.com>
2021-09-12 21:51:22 -0400

    using reactjs/toolkit

--- redux-demo/01-starting-project/src/store/index.js
+++ redux-demo/01-starting-project/src/store/index.js
@@ -1,36 +1,29 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: false};
 
-const counterReducer = (state = initialState , action) => {
-    if(action.type === 'INCR') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        }
-    }
-    if(action.type === 'DECR') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'INCREASE') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        }
-    } 
-    if(action.type === 'VISIBILITY') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        },
 
-    return state;
-}
+    }
+});
 
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
 export default store;
