134695295ba509a84ef55e305a0bd140bc5521e9
Jam3e3333 <jamm3e3333@gmail.com>
2021-09-14 21:29:01 -0400

    using Thunks

--- redux-async/src/App.js
+++ redux-async/src/App.js
@@ -3,9 +3,9 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
 
+import { sendCartData } from './store/cart-slice';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
 
 let isInitial = true;
 
@@ -17,45 +17,11 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const sendCartData = async () => {
-      try{
-        dispatch(uiActions.showNotification({
-          status: 'pending',
-          title: 'sending',
-          message: 'sending cart data',
-        }))
-        const response = await fetch('https://next-js-734e8-default-rtdb.firebaseio.com/cart.json', {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        });
-        if(!response.ok) {
-          throw new Error();
-        }
-        const data = await response.json();
-        if(!Object.keys(data)) {
-          throw new Error();
-        }
-        dispatch(uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cat data successfully!',
-        }));
-
-      }
-      catch(e) {
-        dispatch(uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Data was not sent!',
-        }));
-      }
-    }
-
     if(isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData();
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   
   return (
